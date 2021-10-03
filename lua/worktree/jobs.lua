cc72accef16c532f04d800e47f805b011bcec2b6
Tami <65782666+tami5@users.noreply.github.com>
2021-10-03 19:48:08 +0300

    refa(*): structure and organization
    + todos
    + fixes

--- lua/worktree/jobs.lua
+++ lua/worktree/jobs.lua
@@ -1,7 +1,9 @@
 local branch_fmt = require "worktree.fmt"
-local assert = require "worktree.assert"
-local util = require "worktree.util"
-local jobs = {}
+local msgs = require "worktree.msgs"
+local M = { assert = {}, get = {}, set = {}, perform = {} }
+local assert, get, set, perform = M.assert, M.get, M.set, M.perform
+
+---TODO: refactor, move assert stuff to assert? and have three main sections of jobs: set, get, perform
 
 local Job = function(o)
   local job = require("plenary.job"):new(o)
@@ -11,109 +13,63 @@ local Job = function(o)
   return job
 end
 
-local notify = util.notify {
-  checkout = { "Failed to checkout/create %s: %s", "checked out/created %s" },
-  merge_remote = { "Failed to merge %s/%s/: %s" },
-  merge = { "Failed to merge %s: %s", "Squashed and merged successfully %s" },
-  get_name = { "Failed to get current branch name of %s: %s" },
-  get_description = { "Creating new Description" },
-  set_name = { "Failed to update name from '%s' to '%s': %s", "Updated name from '%s' to '%s'" },
-  set_description = { "Failed to update branch description to %s: %s" },
-  pr_open = { "Failed to create a pull request: %s", "pull request has been created successfully." },
-  pr_update_body = { "Failed to update pr body for %s: ", "pr body has been updated successfully" },
-  pr_update_title = { "Failed to update pr title for %s: ", "pr title has been updated successfully" },
-  pr_get_info = { "Failed to get pr info for %s: " },
-  pr_sync_info = { "Failed to sync pr info for %s: ", "PR is successfully synced!!" },
-  sync_current_info = { "Failed to sync branch/pr info for %s: " },
-  get_current_info = { "Failed to get branch/pr info for %s: " },
-  offline_save = { "Unable to sync changes to remote PR. Saving locally instead.", "Syncing changes to remote ..." },
-}
+---Check whether a branch has a upstream/origin repo
+---@param cwd string
+---@return Job
+get.remotes = function(cwd)
+  return Job {
+    "git",
+    "remote",
+    cwd = cwd,
+    on_exit = function(j, _)
+      local res = j._stdout_results
+      local ret = {}
+      for _, value in ipairs(res) do
+        ret[value] = true
+      end
+      j._stdout_results = ret
+    end,
+  }
+end
 
-jobs.iserr = function(code, type)
-  if code == 0 then
-    return false
+---Get remote name. Here is hacky way of doing it. check if upstream passes, if so make upstream.
+---@param cwd string
+---@return string
+get.remote_name = function(cwd)
+  local remotes = get.remotes(cwd):sync()
+  if remotes.upstream then
+    return "upstream"
+  elseif remotes.origin then
+    return "origin"
   end
-  notify(type)
-  return false
 end
 
-jobs.has_orign = function(cwd)
-  return Job { "git", "config", "remote.upstream.url", cwd = cwd }
+---Get current branch name
+---@param cwd string
+---@return Job
+get.name = function(cwd)
+  return Job { "git", "rev-parse", "--abbrev-ref", "HEAD", cwd = cwd, on_exit = msgs.get_name(cwd) }
 end
 
-jobs.get_remote = function(cwd)
-  local remote = "origin"
-  local args = { "git", "config", "remote.upstream.url", cwd = cwd, sync = true }
-  args.on_exit = function(_, c)
-    if c == 0 then
-      remote = "upstream"
-    end
-  end
-
-  Job(args)
-  return remote
-end
-
-jobs.fetch = function(cwd) -- used in case of commits and tags_to_string
-  local args = { "git", "fetch", "--depth=999999", "--progress", cwd = cwd }
-  return Job(args)
-end
-
-jobs.push = function(branch_name, cwd)
-  local args = { "git", "push", "-u", jobs.get_remote(cwd), branch_name, cwd = cwd }
-  return Job(args)
-end
-
-jobs.checkout = function(branch_name, cwd)
-  local base = (branch_name == "master" or branch_name == "main")
-  local args = base and { "git", "checkout", branch_name, cwd = cwd }
-    or { "git", "checkout", "-b", branch_name, cwd = cwd }
-  args.on_exit = notify("checkout", branch_name)
-  return Job(args)
-end
-
-jobs.merge_remote = function(branch_name, cwd)
-  local remote = jobs.get_remote(cwd)
-  local args = { "git", "merge", remote, branch_name, cwd = cwd }
-  args.on_exit = notify("merge_remote", remote, branch_name)
-  return Job(args)
-end
-
-jobs.merge = function(branch_name, cwd)
-  local args = { "git", "merge", "--squash", branch_name, cwd = cwd }
-  args.on_exit = notify("merge", branch_name)
-  return Job(args)
-end
-
-jobs.get_name = function(cwd)
-  local args = { "git", "rev-parse", "--abbrev-ref", "HEAD", cwd = cwd }
-  args.on_exit = notify("get_name", cwd)
-  return Job(args)
-end
-
-jobs.get_title = function(name)
-  return branch_fmt.into_title(name)
-end
-
-jobs.has_remote = function(branch_name, cwd)
-  -- error(vim.inspect(branch_name))
-  --- Could origin be something else in every day use?
-  local args = { "git", "branch", "-r", "--list", "origin/" .. branch_name, sync = true, cwd = cwd }
-  local remote = Job(args)
-  return remote[1] ~= nil
-end
-
-jobs.get_description = function(branch_name, cwd)
+---Get branch description
+---@param branch_name string
+---@param cwd string
+---@return Job
+get.description = function(branch_name, cwd)
   local path = string.format("branch.%s.description", branch_name)
   local args = { "git", "config", path, cwd = cwd }
-  args.on_exit = notify("get_description", cwd)
+  args.on_exit = msgs.get_description(cwd)
   return Job(args)
 end
 
-jobs.get_commits = function(branch_name, cwd)
+---Get current branch commits
+---@param branch_name string
+---@param cwd string
+---@return Job
+get.commits = function(branch_name, cwd)
   local commits = {}
   local curidx = 0
-  local base = "master" --- TODO: get base branch automatically
+  local base = get.default_branch_name(assert.has_remote(cwd), cwd)
   local range = string.format("%s..%s", base, branch_name)
   local args = { "git", "log", range, "--oneline", "--reverse", "--format=X<<%s%n%n%b", cwd = cwd }
   args.on_stdout = function(_, line)
@@ -130,19 +86,36 @@ jobs.get_commits = function(branch_name, cwd)
   return Job(args)
 end
 
---- TODO: (updating branch name) should fail when remote branch exists or update remote??
-jobs.set_name = function(branch_name, new, cwd)
-  local args = { "git", "branch", "-m", branch_name, new, cwd = cwd }
-  args.on_exit = notify("set_name", branch_name, new)
-
-  if cwd == vim.loop.cwd() then
-    vim.g.gitsigns_head = new
-    vim.api.nvim_buf_set_var(0, "gitsigns_head", new)
+get.default_branch_name = function(has_remote, cwd)
+  local default = ""
+  if has_remote then
+    default = vim.loop.fs_stat(cwd .. "/.git/refs/heads/master") and "master" or "main"
+  else
+    local res, _ = Job { "git", "config", "--global", "init.defaultBranch", sync = true }
+    default = res[1]
   end
-  return Job(args)
+
+  return I(default)
 end
 
-jobs.is_branch = function(branch_name, cwd)
+---Check if user is connect to the network
+---@param as_job any
+---@return boolean
+assert.is_online = function(as_job)
+  local check = { "ping", "-c", "1", "github.com" }
+  if not as_job then
+    check.sync = true
+    local _, code = Job(check)
+    return code == 0
+  end
+  return Job(check)
+end
+
+---Check if a given branch_name exists
+---@param branch_name string
+---@param cwd string
+---@return Job
+assert.is_branch = function(branch_name, cwd)
   local args = { "git", "show-ref", "--quiet", "refs/heads/" .. branch_name, cwd = cwd }
   args.on_exit = function(j, code)
     j._stdout_results = code ~= 0
@@ -150,54 +123,154 @@ jobs.is_branch = function(branch_name, cwd)
   return Job(args)
 end
 
-jobs.set_description = function(branch_name, description, cwd)
-  local path = string.format("branch.%s.description", branch_name)
-  local args = { "git", "config", path, description, cwd = cwd }
-  args.on_exit = notify("set_description", description)
+---Check if a {branch_name} has remote version.
+---@param branch_name string
+---@param cwd any
+---@return boolean
+assert.has_origin_version = function(branch_name, cwd)
+  --- TODO: Could origin be something else in every-day use?
+  local args = { "git", "branch", "-r", "--list", "origin/" .. branch_name, sync = true, cwd = cwd }
+  local remote = Job(args)
+  return remote[1] ~= nil
+end
+
+---check if repo has remote branch
+---@param cwd string
+assert.has_remote = function(cwd)
+  local name = get.remote_name(cwd)
+  return not name and false or true
+end
+
+---Merge remote change for a branch name if any, skip otherwise
+--TODO: make it skip if not remote branch
+---@param branch_name string
+---@param cwd string
+---@return Job
+perform.merge_remote = function(branch_name, cwd)
+  local remote_name = get.remote_name(cwd)
+  local args = { "git", "merge", remote_name, branch_name, cwd = cwd }
+  args.on_exit = msgs.merge_remote(remote_name, branch_name)
   return Job(args)
 end
 
-jobs.commit = function(title, body, cwd)
-  local args = { "git", "commit", "-m", title, cwd = cwd }
+---Squash and merge commits from a given branch_name.
+---TODO: make it accept target
+---@param branch_name string
+---@param cwd string
+---@return Job
+perform.squash_and_merge = function(branch_name, cwd)
+  local args = { "git", "merge", "--squash", branch_name, cwd = cwd }
+  args.on_exit = msgs.squash_and_merge(branch_name)
+  return Job(args)
+end
+
+---Fetch new changes from remote.
+-- TODO: make it ignore fetching if not remote is avaliable
+-- TODO: which remote?
+---@param cwd any
+---@return Job
+perform.fetch = function(cwd)
+  local args = { "git", "fetch", "--depth=999999", "--progress", cwd = cwd }
+  return Job(args)
+end
+
+---Push changes for a given branch_name to remote
+--TODO: catch and prit errors. Also ignore if no remote is avaliable.
+---@param branch_name string
+---@param cwd string
+---@return Job
+perform.push = function(branch_name, cwd)
+  local remote = get.remote_name(cwd)
+  local args = { "git", "push", "-u", remote, branch_name, cwd = cwd, on_exit = msgs.push(branch_name) }
+  return Job(args)
+end
+
+---Checkout a given branch name. If branch name is master or main then then simply switch?
+---@param branch_name string
+---@param cwd string
+---@return Job
+perform.checkout = function(branch_name, cwd)
+  local base = (branch_name == "master" or branch_name == "main")
+  local args = base and { "git", "checkout", branch_name, cwd = cwd }
+    or { "git", "checkout", "-b", branch_name, cwd = cwd }
+  args.on_exit = msgs.checkout(branch_name)
+  return Job(args)
+end
+
+---Update branch name
+---@param branch_name string
+---@param new string
+---@param cwd string
+---@return Job
+set.name = function(branch_name, new, cwd)
+  --- TODO: (updating branch name) should either fail when remote branch exists or update remote local and remote
+  local args = { "git", "branch", "-m", branch_name, new, cwd = cwd }
+  args.on_exit = msgs.set_name(branch_name, new)
+
+  if cwd == vim.loop.cwd() then -- FIXME: this doesn't always work
+    vim.g.gitsigns_head = new
+    vim.api.nvim_buf_set_var(0, "gitsigns_head", new)
+  end
+  return Job(args)
+end
+
+---Update branch local description
+---@param branch_name string
+---@param description string
+---@param cwd string
+---@return Job
+set.description = function(branch_name, description, cwd)
+  local path = string.format("branch.%s.description", branch_name)
+  return Job { "git", "config", path, description, cwd = cwd, on_exit = msgs.set_description(description) }
+end
+
+---Make a commit
+---@param heading string @commit heading
+---@param body string @commit body
+---@param cwd string @path where the git command should be executed
+---@return Job
+perform.commit = function(heading, body, cwd)
+  local args = { "git", "commit", "-m", heading, cwd = cwd }
   body = vim.split(body, "\n")
   for _, line in ipairs(body) do
-    args[#args + 1] = "-m"
-    args[#args + 1] = line
+    if line ~= "" then
+      args[#args + 1] = "-m"
+      args[#args + 1] = line
+    end
   end
 
   return Job(args)
 end
 
-jobs.repo_fork = function(cwd)
-  return Job { "gh", "repo", "fork", "--remote=true", cwd = cwd }
+---Job to fork remote repo to origin and change current to upstream
+---@param cwd string
+---@return Job
+perform.fork = function(cwd)
+  return Job { "gh", "repo", "fork", "--remote=true", cwd = cwd, on_exit = msgs.fork(cwd) }
 end
 
 ---Job to get pr info
 ---@param cwd string
 ---@return Job
-jobs.pr_get_info = function(cwd)
+get.pr_info = function(cwd)
   return Job { "gh", "pr", "view", "--json", "title", "--json", "body", cwd = cwd }
 end
 
 ---Job to update info
 ---@param cwd string
 ---@return Job
-jobs.pr_update = function(fields, cwd)
+set.pr_info = function(fields, cwd)
   local start = assert.is_online(true)
   local cb = fields.cb
   fields.cb = nil
-  local args = { "gh", "pr", "edit", on_exit = notify("pr_sync_info", cwd) }
+  local args = { "gh", "pr", "edit", on_exit = msgs.pr_update(cwd) }
   for field, value in pairs(fields) do
     table.insert(args, "--" .. field)
     table.insert(args, value)
   end
   local update = Job(args)
 
-  start:after_failure(vim.schedule_wrap(function(_, code)
-    if jobs.iserr(code, "is_online") then
-      return
-    end
-  end))
+  start:after_failure(msgs.offline_save())
   start:and_then_on_success(update)
   if cb then
     update:after_success(vim.schedule_wrap(cb))
@@ -210,10 +283,8 @@ end
 ---@param cwd string
 ---@return Job
 -- fails when the branch is already connected to a pull request
-jobs.pr_open = function(title, body, cwd)
-  local args = { "gh", "pr", "create", "--title", title, "--body", body, cwd = cwd }
-  args.on_exit = notify "pr_open"
-  return Job(args)
+perform.pr_open = function(title, body, cwd)
+  return Job { "gh", "pr", "create", "--title", title, "--body", body, cwd = cwd, on_exit = msgs.pr_open() }
 end
 
-return jobs
+return M
